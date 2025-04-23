b3d99b8c9997555228c2ef5cd694e470d6fbea24
G-jjunny <rudwns9551@naver.com>
2025-04-23 14:57:21 +0900

    feat: navigate추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,10 @@
+import { ROUTES } from "@/shared/contants/routes";
 import { Button } from "@/shared/ui/shadcn/button";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className=" flex flex-col gap-5">
       <h3 className=" text-2xl font-bold">Please select a menu</h3>
@@ -8,6 +12,7 @@ const HomePage = () => {
         <Button
           variant="link"
           className=" h-[100px] bg-[#415A77] font-bold text-md text-white cursor-pointer"
+          onClick={() => navigate(ROUTES.REGISTER)}
         >
           Register
           <br />
@@ -18,6 +23,7 @@ const HomePage = () => {
         <Button
           variant="link"
           className="h-[100px] bg-[#415A77] font-bold text-md text-white cursor-pointer"
+          onClick={() => navigate(ROUTES.LIST)}
         >
           Enrollment
           <br />
