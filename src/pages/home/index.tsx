9f303630a3d12c0cf8f2e4750cfc04e0573af2ae
G-jjunny <rudwns9551@naver.com>
2025-04-23 14:55:20 +0900

    feat: hompage UI구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,31 @@
+import { Button } from "@/shared/ui/shadcn/button";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" flex flex-col gap-5">
+      <h3 className=" text-2xl font-bold">Please select a menu</h3>
+      <div className=" flex gap-5">
+        <Button
+          variant="link"
+          className=" h-[100px] bg-[#415A77] font-bold text-md text-white cursor-pointer"
+        >
+          Register
+          <br />
+          New
+          <br />
+          Participant
+        </Button>
+        <Button
+          variant="link"
+          className="h-[100px] bg-[#415A77] font-bold text-md text-white cursor-pointer"
+        >
+          Enrollment
+          <br />
+          List
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
